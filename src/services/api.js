0fdc801203cc6657b8262695e0cf5c4725f1feb5
ubaidullah-code <ubaidque14@gmail.com>
2026-05-09 13:39:50 +0500

    build

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'https://first-restuarant.vercel.app';
 
 const api = axios.create({
   baseURL: 'https://first-restuarant.vercel.app',
